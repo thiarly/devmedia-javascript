86a9b3b768c61c6301f406211849c6aea52d6814
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-09-18 16:10:09 -0300

    Create array.js

---
+++ javaScript/array.js
@@ -0,0 +1,15 @@
+var estados = ['canudos', 'uaua', 'juazeiro']
+
+console.log(estados[1])
+
+var linguagens = ["JavaScript", "PHP", "JAVA", "C#"];
+
+// Imprimindo o valor da posição 0 do array linguagens
+console.log(linguagens[0]);
+
+// Alterando o valor da posição 2.
+linguagens[2] = "C";
+
+// Imprimindo o valor alterado
+console.log(linguagens[2]);
+
